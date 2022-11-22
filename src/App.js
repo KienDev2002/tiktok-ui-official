67910fa604845e7fa3f38774564c092e3583166d
KienDev <ngotrungkien2k2@gmail.com>
2022-11-22 23:15:32 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React ae f8 oi aaa
         </a>
       </header>
     </div>
